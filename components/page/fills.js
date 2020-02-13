d42c2142251d6df8088a858f17e09872816ceb5f
Benjamin <benjamin@SAILcloud-MBP.local>
2020-02-13 11:30:47 +0100

    fix/list scrolling

--- components/page/fills.js
+++ components/page/fills.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { View, Text } from "react-native";
-import { Icon, ListItem, colors } from "react-native-elements";
+import { View, Text, ScrollView } from "react-native";
+import { Icon, ListItem } from "react-native-elements";
 import FAB from "react-native-fab";
 import AddFillModal from "../modal/addFill";
 import styles from "./styles";
@@ -29,40 +29,42 @@ class FillsPage extends React.Component {
           printModal={this.state.isAddFillModalVisible}
           onDisapearCallback={this.toggleAddFillModal}
         />
-        {this.props.fills.map(item => (
-          <ListItem
-            key={item.mileage}
-            title={
-              <View style={styles.row}>
-                <View style={styles.flex3}>
-                  <Text style={styles.fontSize20}>{item.mileage} km</Text>
-                  <Text style={styles.grey}>{item.date}</Text>
+        <ScrollView>
+          {this.props.fills.map(item => (
+            <ListItem
+              key={item.mileage}
+              title={
+                <View style={styles.row}>
+                  <View style={styles.flex3}>
+                    <Text style={styles.fontSize20}>{item.mileage} km</Text>
+                    <Text style={styles.grey}>{item.date}</Text>
+                  </View>
+                  <View style={styles.fillDescLeft}>
+                    <Text style={styles.textAlignRight}>
+                      {item.pricePerLitre}
+                    </Text>
+                    <Text style={styles.textAlignRight}>{item.amount}</Text>
+                    <Text style={styles.textAlignRight}>
+                      {item.consumption ? item.consumption : "? "}
+                    </Text>
+                  </View>
+                  <View style={styles.fillDescRight}>
+                    <Text style={[styles.grey, styles.textAlignLeft]}>
+                      | € / litre
+                    </Text>
+                    <Text style={[styles.grey, styles.textAlignLeft]}>
+                      | € total
+                    </Text>
+                    <Text style={[styles.grey, styles.textAlignLeft]}>
+                      | L / 100km
+                    </Text>
+                  </View>
                 </View>
-                <View style={styles.fillDescLeft}>
-                  <Text style={styles.textAlignRight}>
-                    {item.pricePerLitre}
-                  </Text>
-                  <Text style={styles.textAlignRight}>{item.amount}</Text>
-                  <Text style={styles.textAlignRight}>
-                    {item.consumption ? item.consumption : "? "}
-                  </Text>
-                </View>
-                <View style={styles.fillDescRight}>
-                  <Text style={[styles.grey, styles.textAlignLeft]}>
-                    | € / litre
-                  </Text>
-                  <Text style={[styles.grey, styles.textAlignLeft]}>
-                    | € total
-                  </Text>
-                  <Text style={[styles.grey, styles.textAlignLeft]}>
-                    | L / 100km
-                  </Text>
-                </View>
-              </View>
-            }
-            bottomDivider
-          />
-        ))}
+              }
+              bottomDivider
+            />
+          ))}
+        </ScrollView>
         <FAB
           buttonColor="#F4FF81"
           iconTextColor="#FFFFFF"
